3c81e47b08d7d579c1005cb2e410d7f83fef9f87
Propfend <mellomello2030@gmail.com>
2025-10-17 13:56:23 +0000

    typo, unnnecessary traits and format.

--- src/sends_rpc_message.rs
+++ src/sends_rpc_message.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use serde::Serialize;
 
 use crate::rpc_message::RpcMessage;
 
 #[async_trait]
 pub trait SendsRpcMessage {
-    type Message: RpcMessage + Send + Serialize;
+    type Message: RpcMessage;
 
     async fn send_rpc_message(&self, message: Self::Message) -> Result<()>;
 }
