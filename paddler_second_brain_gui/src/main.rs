51b7d8f3a506df99a85cd16254d69723ae52276d
Małgorzata Zagajewska <m.zagajewska@gmail.com>
2026-03-16 20:15:47 +0100

    create a minimal PoC with iced

---
+++ paddler_second_brain_gui/src/main.rs
@@ -0,0 +1,30 @@
+use iced::Center;
+use iced::widget::{Column, button, column};
+
+fn main() -> iced::Result {
+    iced::run(SecondBrain::update, SecondBrain::view)
+}
+
+#[derive(Default)]
+struct SecondBrain;
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    ButtonPressed,
+}
+
+impl SecondBrain {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonPressed => {}
+        }
+    }
+
+    fn view<'view>(&'view self) -> Column<'view, Message> {
+        column![
+            button("Hello from Paddler").on_press(Message::ButtonPressed),
+        ]
+        .padding(20)
+        .align_x(Center)
+    }
+}
