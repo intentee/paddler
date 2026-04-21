6d7d7f760564428d4883a0e54d664a9ba413f5c5
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2026-04-21 22:28:19 +0200

    simplify paddler shutdown

--- paddler/src/service_manager.rs
+++ paddler/src/service_manager.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use actix_web::rt;
 use anyhow::Result;
 use futures::future::join_all;
@@ -22,35 +20,18 @@ impl ServiceManager {
 
     pub async fn run_forever(self, shutdown_rx: oneshot::Receiver<()>) -> Result<()> {
         let (shutdown_broadcast_tx, _) = broadcast::channel::<()>(1);
-        let shutdown_broadcast_tx_arc = Arc::new(shutdown_broadcast_tx.clone());
         let mut service_handles = Vec::with_capacity(self.services.len());
 
         for mut service in self.services {
             let service_name = service.name().to_owned();
-            let shutdown_broadcast_tx_arc_clone = shutdown_broadcast_tx_arc.clone();
+            let service_shutdown_rx = shutdown_broadcast_tx.subscribe();
 
             service_handles.push(rt::spawn(async move {
                 info!("{service_name}: Starting");
 
-                let mut manager_shutdown_rx = shutdown_broadcast_tx_arc_clone.subscribe();
-                let service_shutdown_rx = shutdown_broadcast_tx_arc_clone.subscribe();
-
-                tokio::select! {
-                    _ = manager_shutdown_rx.recv() => {
-                        info!("{service_name}: Received shutdown signal");
-                    }
-                    result = service.run(service_shutdown_rx) => {
-                        match result {
-                            Ok(()) => {
-                                info!("{service_name}: Stopped");
-
-                                if let Err(err) = shutdown_broadcast_tx_arc_clone.send(()) {
-                                    error!("{service_name}: Failed to send shutdown signal: {err}");
-                                }
-                            }
-                            Err(err) => error!("{service_name}: {err}"),
-                        }
-                    }
+                match service.run(service_shutdown_rx).await {
+                    Ok(()) => info!("{service_name}: Stopped"),
+                    Err(err) => error!("{service_name}: {err}"),
                 }
             }));
         }
